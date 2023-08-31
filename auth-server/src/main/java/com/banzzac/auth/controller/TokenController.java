d7df71d5ebd6b6248d267ab7045ee27a38192ec1
kdj9878 <kdj9878@gmail.com>
2023-08-31 22:24:17 +0900

    Ref: 서비스에 불필요한 테스트 코드 제거
    - 토큰 생성 메소드 제거
    - @RequiredArgsConstructor 추가를 통한 생성자 제거

--- auth-server/src/main/java/com/banzzac/auth/controller/TokenController.java
+++ auth-server/src/main/java/com/banzzac/auth/controller/TokenController.java
@@ -1,9 +1,9 @@
 package com.banzzac.auth.controller;
 
-import com.banzzac.auth.domain.TokenInfo;
 import com.banzzac.auth.service.AuthenticationService;
 import com.banzzac.core.common.response.CommonResponse;
 import com.banzzac.core.common.response.ErrorCode;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,19 +14,10 @@ import javax.servlet.http.HttpServletRequest;
  */
 @RestController
 @RequestMapping("/token")
+@RequiredArgsConstructor
 public class TokenController {
 
     private final AuthenticationService authenticationService;
-
-    public TokenController(AuthenticationService authenticationService) {
-        this.authenticationService = authenticationService;
-    }
-
-    @GetMapping
-    public ResponseEntity createTokenTest(){
-        TokenInfo token = authenticationService.createTokenInfo(1L);
-        return ResponseEntity.ok(token);
-    }
 
     @PostMapping("/check")
     public ResponseEntity validateToken(HttpServletRequest request){
